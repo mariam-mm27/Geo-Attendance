24f19d428733d7e16a67cd374fb327f775f95637
MariamHany2327237 <mariamhany31017@gmail.com>
2026-03-04 21:08:47 +0200

    security and role validation

--- frontend/Web/src/pages/Student.js
+++ frontend/Web/src/pages/Student.js
@@ -1,4 +1,14 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
 function Student() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    navigate("/"); 
+  };
   return <h1>Student Dashboard</h1>;
 }
 
--- frontend/mobile/AttendanceClean/app/screens/StudentHomeScreen.tsx
+++ frontend/mobile/AttendanceClean/app/screens/StudentHomeScreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 export default function StudentHomeScreen() {
+  const { setUser, setRole } = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    setUser(null);
+    setRole(null);
+  };
+
   return (
     <View style={{ padding: 20 }}>
       <Text>Hello this is the student home page</Text>
+
     </View>
-  )
+  );
 }
