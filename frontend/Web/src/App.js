23df05d60df6b5b41ae54607ddeff388d523b294
Mariam Khaled <mariam22khalid22@gmail.com>
2026-03-06 08:29:02 +0200

    Fix white page issue by removing react-hot-toast conflicts

--- frontend/Web/src/App.js
+++ frontend/Web/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 
 import Login from "./Login";
 import Register from "./Register";
@@ -9,29 +8,25 @@ import ResetPasswordPage from "./pages/ResetPasswordPage";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import ProfessorProfile from "./pages/Professor/ProfessorProfile";
 import StudentProfile from "./pages/Student/StudentProfile";
-import CourseDetails from "./pages/admin/CourseDetails"; // اتأكدي إن الملف ده موجود
+import CourseDetails from "./pages/admin/CourseDetails";
 
 function App() {
   return (
-    <>
-      <Toaster position="top-right" />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/professor" element={<ProfessorProfile />} />
-          <Route path="/student" element={<StudentProfile />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/reports/:courseId" element={<Reports />} />
-          
-          {}
-          <Route path="/details/:type/:id" element={<CourseDetails />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/professor" element={<ProfessorProfile />} />
+        <Route path="/student" element={<StudentProfile />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/reports/:courseId" element={<Reports />} />
+        
+        <Route path="/details/:type/:id" element={<CourseDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
