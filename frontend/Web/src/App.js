cd72be2527c65aa4ffd06e8de8dd91318f3b8050
Mariam Khaled <mariam22khalid22@gmail.com>
2026-03-06 18:33:59 +0200

    Fix merge conflicts in package.json and App.js

--- frontend/Web/src/App.js
+++ frontend/Web/src/App.js
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // غيرت دي عشان تمشي مع الكود تحت
-import { AuthProvider } from "./utils/AuthContex";
-import { Toaster } from 'react-hot-toast';
-import PrivateRoute from "./utils/PrivateRoute";
->>>>>>> Stashed changes
 
 import Login from "./Login";
 import Register from "./Register";
@@ -19,7 +12,6 @@ import CourseDetails from "./pages/admin/CourseDetails";
 
 function App() {
   return (
-<<<<<<< Updated upstream
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -35,27 +27,6 @@ function App() {
         <Route path="/details/:type/:id" element={<CourseDetails />} />
       </Routes>
     </Router>
-=======
-    <AuthProvider> {}
-      <Toaster position="top-right" />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          
-          {}
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/professor" element={<ProfessorProfile />} />
-          <Route path="/student" element={<StudentProfile />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/reports/:courseId" element={<Reports />} />
-          
-          <Route path="/details/:type/:id" element={<CourseDetails />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
->>>>>>> Stashed changes
   );
 }
 
