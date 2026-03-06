ee3b9afcb28b1b7b020f14b038ec1af4038714ba
Mariam Khaled <mariam22khalid22@gmail.com>
2026-03-06 09:24:24 +0200

    solve errors & conflicts

--- frontend/Web/src/App.js
+++ frontend/Web/src/App.js
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+=======
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // غيرت دي عشان تمشي مع الكود تحت
+import { AuthProvider } from "./utils/AuthContex";
+import { Toaster } from 'react-hot-toast';
+import PrivateRoute from "./utils/PrivateRoute";
+>>>>>>> Stashed changes
 
 import Login from "./Login";
 import Register from "./Register";
@@ -12,6 +19,7 @@ import CourseDetails from "./pages/admin/CourseDetails";
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -27,6 +35,27 @@ function App() {
         <Route path="/details/:type/:id" element={<CourseDetails />} />
       </Routes>
     </Router>
+=======
+    <AuthProvider> {}
+      <Toaster position="top-right" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {}
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/professor" element={<ProfessorProfile />} />
+          <Route path="/student" element={<StudentProfile />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/reports/:courseId" element={<Reports />} />
+          
+          <Route path="/details/:type/:id" element={<CourseDetails />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+>>>>>>> Stashed changes
   );
 }
 
