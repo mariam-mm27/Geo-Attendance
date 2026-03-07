79491a86b896858e27aa9c2aefe5085d350caa49
Mariam Khaled <mariam22khalid22@gmail.com>
2026-03-07 05:59:09 +0200

    Fix App.js and firebase.js merge conflicts in Web

--- frontend/Web/src/App.js
+++ frontend/Web/src/App.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./utils/AuthContex";
-import PrivateRoute from "./utils/PrivateRoute";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Admin from "./pages/Admin";
-import Student from "./pages/Student";
-import Professor from "./pages/Professor";
-import Profile from "./pages/Profile";
-
-function App() {
-  return (
-    <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-          Security
-         <Route
-            path="/admin"
-            element={
-              <PrivateRoute allowedRoles={["admin"]}>
-                <Admin />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path="/student"
-            element={
-              <PrivateRoute allowedRoles={["student"]}>
-                <Student />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path="/professor"
-            element={
-              <PrivateRoute allowedRoles={["professor"]}>
-                <Professor />
-              </PrivateRoute>
-            }
-          />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/student" element={<Student />} />
-        <Route path="/professor" element={<Professor />} />
-        <Route path="/profile" element={<Profile />} />
- main
-      </Routes>
-    </BrowserRouter>
-    </AuthProvider>
-  );
-}
-
-export default App;
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -92,4 +33,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 8b9578bc1e2302e3a75ac27510a86a584aaa425f
