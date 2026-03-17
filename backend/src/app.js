9c56dcb8f856af80cd3c6763b48b1ddde167670e
Mariam Khaled <mariam22khalid22@gmail.com>
2026-03-17 06:19:18 +0200

    Add backend course management: updateCourse, deleteCourse, getCourseSessions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./routes/user.routes.js";
 import enrollmentRouter from "./routes/enrollment.routes.js";
+import courseRoutes from "./routes/course.routes.js";
 import { attendanceControllerTest } from "./controllers/attendanceController.js";
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use("/", userRoutes);
 app.use("/api/enrollment", enrollmentRouter);
+app.use("/api/courses", courseRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
